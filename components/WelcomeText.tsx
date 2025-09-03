ed56d49af8ae3742913f4760ac967dd9fd9668d7
vgmiltonisaac <vgmiltonisaac@gmail.com>
2025-09-03 21:17:40 +0000

    ✨feat(WelcomeText): Añadir Componente - Crear componente de WelcomeText - Añadir a index.tsx

---
+++ components/WelcomeText.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export function WelcomeText():any {
+    return (    
+    <View>
+        <Text>Bienvenido a mi app</Text>
+        <Text>PEPE</Text>
+    </View>
+    );
+}
